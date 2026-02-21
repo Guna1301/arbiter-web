ccb30704918c03079f14551933b3a8aaeac940d1
Guna1301 <gskotipalli2005@gmail.com>
2026-02-21 16:24:51 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-semibold">
+      Arbiter Dashboard
+    </div>
+  );
+}
+
+export default App
